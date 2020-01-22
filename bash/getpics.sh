4733a82dcb86357054dbd5d8688d42819b5bfad7
Graham <100152886@student.georgianc.on.ca>
2020-01-22 13:57:43 -0800

    getpics.sh final

--- bash/getpics.sh
+++ bash/getpics.sh
@@ -11,9 +11,9 @@
 # make a Pictures directory if we don't have one - assumes we have a home directory
 test -d ~/Pictures || mkdir ~/Pictures
 
-#######################
+##################################################################
 #handle the zip file
-#######################
+###################################################################
 
 
 # download a zipfile of pictures to our Pictures directory if it isn't already there - assumes you are online
@@ -22,14 +22,18 @@ test -f ~/Pictures/pics.zip || wget -q -O ~/Pictures/pics.zip http://zonzorp.net
 # unpack the downloaded zipfile if it is there, then delete the local copy of the zipfile
 test -f ~/Pictures/pics.zip && unzip -d ~/Pictures -o -q ~/Pictures/pics.zip && rm ~/Pictures/pics.zip
 
-################
+###################################################################
 # handle the tar file
-################
+####################################################################
+
 # download the tar file if we need it
 test -f ~/Pictures/pics.tgz || wget -q -O ~/Pictures/pics.tgz http://zonzorp.net/pics.tgz
 
 # unpack the tar file and remove it if successful
-test -f ~/Pictures/pics.tgz && tar -xf ~/Pictures/pics.tgz -C ~/Pictures && rm ~/Pictures/pics.tgz
+test -f ~/Pictures/pics.tgz && tar -xf ~/Pictures/pics.tgz ~/Pictures && rm ~/Pictures/pics.tgz
+
+########################################################################
+
 
 # Make a report on what we have in the Pictures directory
 test -d ~/Pictures && cat <<EOF
