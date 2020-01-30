9a42e9f19c7e03603f826c95260adad2c4ff8c37
Graham <100152886@student.georgianc.on.ca>
2020-01-30 10:05:06 -0800

    forgot to add rolls for length of name

--- bash/diceroller.sh
+++ bash/diceroller.sh
@@ -6,6 +6,19 @@
 # Task: Improve this script by making it roll as many dice as there are letters in your first name, instead of just one
 
 # roll the dice and display the result
+read -p "What is your name?: " name
+
+length=${#name}
+
+roll=0
+
+echo "Your name has $length letters. Rolling dice $length times:"
+
+while [[ "$roll" < "$length" ]]; do
+  echo "$(( RANDOM % 6 + 1))"
+  let roll=$roll+1
+done
+
 echo "
 Rolling six-sided dice....
 $(( RANDOM % 6 + 1)) rolled.
